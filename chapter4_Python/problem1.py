2a4556a6829ff42dcb225aefd4e59ae16bd88510
Akshay Gogate <akshaygogate@Akshays-Laptop.local>
2024-07-03 13:48:27 +0530

    Newly Added FIles

---
+++ chapter4_Python/problem1.py
@@ -0,0 +1,18 @@
+fruits = []
+
+f1 = input("Enter you fruit: ")
+fruits.append(f1)
+
+f2 = input("Enter you fruit: ")
+fruits.append(f2)
+
+f3 = input("Enter you fruit: ")
+fruits.append(f3)
+
+f4 = input("Enter you fruit: ")
+fruits.append(f4)
+
+f5 = input("Enter you fruit: ")
+fruits.append(f5)
+
+print(fruits)
---
+++ chapter4_Python/problem3.py
@@ -0,0 +1,5 @@
+a = (34, 234, "akshay")
+
+a[2] = "United"   # Tuple is immutable
+
+print(a)
